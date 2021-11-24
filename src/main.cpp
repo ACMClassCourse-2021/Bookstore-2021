c4608191e5d1a002255ce8aba37d469a3a7e0b84
Frank Qiu <982713785@qq.com>
2021-11-24 19:26:36 +0800

    补充链接库使用说明

--- src/main.cpp
+++ src/main.cpp
@@ -6,8 +6,16 @@
 
 int main() {
     // ! Notice
-    // You must put the dll file and the executable
-    // file in the same directory, or it cannot run.
+    // If you are using dynamic-link library, the DLL
+    // file (filename extension is `.so` in Linux and
+    // `.dll` in Windows) must be in a location that
+    // the executable file can directly access(that is,
+    // under the same folder or under system PATH),
+    // otherwise it cannot run.
+    // But if you are using the statically-linked library
+    // (filename extension is `.a` in Linux and `.lib`
+    // in Windows), the executable file can run without
+    // any other file.
 
     // Initialize ULL with file name
     Ull testUll("test.dat");
